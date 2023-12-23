eba036b62bc5d33a20af269d23afb834dac3deeb
mnenie <121057011+mneniee@users.noreply.github.com>
2023-12-23 22:44:12 +0300

    add: delivery page components

--- src/views/DeliveryPage.vue
+++ src/views/DeliveryPage.vue
@@ -1,10 +1,16 @@
 <script setup lang="ts">
-
+import NavigationTopPage from '@/components/UI/links/NavigationTopPage.vue';
+import DeliveryRequest from '@/components/elements/delivery/DeliveryRequest.vue'
+import BannerDelivery from '@/components/elements/delivery/BannerDelivery.vue'
+import DownloadBannerDelivery from '@/components/elements/delivery/DownloadBannerDelivery.vue';
 </script>
 
 <template>
   <div>
-    deliverypage
+    <NavigationTopPage :title="'Доставка и оплата'" />
+    <BannerDelivery />
+    <DownloadBannerDelivery />
+    <DeliveryRequest />
   </div>
 </template>
 
