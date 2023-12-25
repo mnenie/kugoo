ef44c2857aeb029966648b51942b8b32844baa66
mnenie <121057011+mneniee@users.noreply.github.com>
2023-12-25 20:57:09 +0300

    add: stocks page, add: banner

--- src/views/StocksPage.vue
+++ src/views/StocksPage.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
-
+import BannerStocks from '@/components/elements/stocks/BannerStocks.vue'
+import NavigationTopPage from '@/components/UI/links/NavigationTopPage.vue';
 </script>
 
 <template>
   <div>
-    stocks
+    <NavigationTopPage :title="'Акции'" />
+    <BannerStocks />
   </div>
 </template>
 
