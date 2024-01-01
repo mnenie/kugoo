9838a47d98126f0cf22088d0b7f8aaeb23372c80
mnenie <121057011+mneniee@users.noreply.github.com>
2024-01-01 20:15:57 +0300

    fix: api, axios

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,8 +2,4 @@ import axios from 'axios'
 
 const $api = axios.create()
 
-// $api.interceptors.request.use((config) => {
-//   config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`
-//   return config;
-// });
 export default $api
